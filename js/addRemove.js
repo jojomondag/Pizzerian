f4da7cb0dca1eba6b81534462c3bfbd4cf8ab0b5
jojomondag <josef.nobach@outlook.com>
2021-06-17 12:26:47 +0200

    AddRemove script tillagt.
    Fixat så att man kan lägga till och ta bort pizzor. Men Alert är fullt fixa en box där man kan se Linda.

---
+++ js/addRemove.js
@@ -0,0 +1,14 @@
+let summa = 0;
+
+function change(ammountTooAdd)
+{
+    summa += ammountTooAdd;
+
+    alert(parseInt(summa).toString());
+}
+
+
+    //www.tutorialspoint.com/javascript-sum-function-on-the-click-of-a-button
+    //var tempElement = document.getElementById("Total");
+    //Total.innerHTML = parseInt(fValue.innerHTML) +
+    //parseInt(ammountTooAdd);
